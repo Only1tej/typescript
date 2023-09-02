67173de9454914609a7a3d8d81079f690ad2edae
only1tej <tijanienny3@gmail.com>
2023-09-01 23:05:31 -0700

    New Section

--- dist/Challenge-Part-1/Loops.js
+++ dist/Challenge-Part-1/Loops.js
@@ -5,3 +5,19 @@ let searchHistory = [
     "html css tutorial",
     "calisthenics",
 ];
+for (let searchItem of searchHistory) {
+    searchItem.split("");
+    for (let entry of searchItem) {
+        if (entry === "html" || entry === "css") {
+            console.log("Would you like to learn how to create website for free?");
+        }
+        else {
+            console.log("What do you want to do?");
+        }
+    }
+}
+searchHistory.map((entry) => {
+    if (entry.includes("html") || entry.includes("css")) {
+        console.log("Would you like to learn how to create website for free?");
+    }
+});
--- src/Challenge-Part-1/Loops.ts
+++ src/Challenge-Part-1/Loops.ts
@@ -1,6 +1,27 @@
+//Challenge 1
+//Based on the search history of a user, return would you like to create website if there is a keyword HTML or CSS
+//LONG VERSION
 let searchHistory: string[] = [
   "how to make money online",
   "benefit of eating gralic",
   "html css tutorial",
   "calisthenics",
 ];
+
+for (let searchItem of searchHistory) {
+  searchItem.split("");
+  for (let entry of searchItem) {
+    if (entry === "html" || entry === "css") {
+      console.log("Would you like to learn how to create website for free?");
+    } else {
+      console.log("What do you want to do?");
+    }
+  }
+}
+
+//SHORTER VERSION
+searchHistory.map((entry) => {
+  if (entry.includes("html") || entry.includes("css")) {
+    console.log("Would you like to learn how to create website for free?");
+  }
+});
