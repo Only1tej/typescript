3a3b88c6f4bd7f3dc48022992f9ce2a866b7a6a5
only1tej <tijanienny3@gmail.com>
2023-10-06 17:35:05 -0700

    Challenge

---
+++ src/19-Challenge-part-8/2-Enums.ts
@@ -0,0 +1,48 @@
+//Challenge 1
+//Create a group of five constant for five products and initialize the enums with 1
+
+// enum Products {
+//   "TS Bootcamp" = 1,
+//   "JS Bootcamp",
+//   "Python Bootcamp",
+//   "CSS Layout",
+//   "Flexbox Grid course",
+// }
+
+//Challenge 2
+//Perform an exhaustiveness checking for the previous enum and return their prices.
+
+enum Products {
+  "TS Bootcamp" = 1,
+  "JS Bootcamp",
+  "Python Bootcamp",
+  "CSS Layout",
+  "Flexbox Grid course",
+  //   "Tailwind",
+}
+
+function setCoursePrice(course: Products): string {
+  switch (course) {
+    case Products["TS Bootcamp"]:
+      return "$25.99";
+    case Products["JS Bootcamp"]:
+      return "$29.99";
+    case Products["Python Bootcamp"]:
+      return "$12.55";
+    case Products["CSS Layout"]:
+      return "$10.45";
+    case Products["Flexbox Grid course"]:
+      return "$16.56";
+    default:
+      return courseWarning(course);
+    //   throw new Error(`The ${course} course has no price yet.`);
+  }
+}
+
+function courseWarning(course: never): never {
+  throw new Error(`The ${course} course has no price yet.`);
+}
+
+console.log(setCoursePrice(Products["TS Bootcamp"]));
+console.log(setCoursePrice(Products["JS Bootcamp"]));
+console.log(setCoursePrice(Products["Flexbox Grid course"]));
