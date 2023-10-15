d744f80c38cc57420cc206ca81ab38586259f2fe
only1tej <tijanienny3@gmail.com>
2023-10-15 01:16:35 -0700

    Required

--- dist/25-Mapped-types/5-Readonly.js
+++ dist/25-Mapped-types/5-Readonly.js
@@ -1,6 +1,1 @@
 "use strict";
-const product = {
-    name: "TS",
-    price: 10.99,
-};
-console.log(product);
--- src/25-Mapped-types/5-Readonly.ts
+++ src/25-Mapped-types/5-Readonly.ts
@@ -1,21 +1,21 @@
-interface Product {
-  name: string;
-  price: number;
-}
+// interface Product {
+//   name: string;
+//   price: number;
+// }
 
-type ReadOnlyTYPE<Type> = {
-  readonly [Key in keyof Type]: Type[Key];
-};
+// type ReadOnlyTYPE<Type> = {
+//   readonly [Key in keyof Type]: Type[Key];
+// };
 
 // const product: ReadOnlyTYPE<Product> = {
 //   name: "TS",
 //   price: 10.99,
 // };
-const product: Readonly<Product> = {
-  name: "TS",
-  price: 10.99,
-};
+// const product: Readonly<Product> = {
+//   name: "TS",
+//   price: 10.99,
+// };
 
 // product.name = "JS";
 
-console.log(product);
+// console.log(product);
